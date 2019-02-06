dd98016c658530fe9e9265675380302980d06275
Błażej Sowa <bsowa123@gmail.com>
2019-02-06 18:37:55 +0100

    tr_webui: add linear and angular speed sliders

--- tr_webui/www/scripts/webui.js
+++ tr_webui/www/scripts/webui.js
@@ -1,21 +1,9 @@
 var twist;
-var cmdVel;
-var publishImmidiately = true;
-var robot_IP;
+var speedLinear;
+var speedAngular;
+var cmdVelPub;
 var manager;
-var teleop;
 var ros;
-
-function moveAction(linear, angular) {
-    if (linear !== undefined && angular !== undefined) {
-        twist.linear.x = linear;
-        twist.angular.z = angular;
-    } else {
-        twist.linear.x = 0;
-        twist.angular.z = 0;
-    }
-    cmdVel.publish(twist);
-}
 
 function initVelocityPublisher() {
     // Init message with zero values.
@@ -32,101 +20,120 @@ function initVelocityPublisher() {
         }
     });
     // Init topic object
-    cmdVel = new ROSLIB.Topic({
+    cmdVelPub = new ROSLIB.Topic({
         ros: ros,
         name: '/cmd_vel',
         messageType: 'geometry_msgs/Twist'
     });
     // Register publisher within ROS system
-    cmdVel.advertise();
+    cmdVelPub.advertise();
 }
 
 function initTeleopKeyboard() {
-    // Use w, s, a, d keys to drive your robot
-
-    // Check if keyboard controller was aready created
-    if (teleop == null) {
-        // Initialize the teleop.
-        teleop = new KEYBOARDTELEOP.Teleop({
-            ros: ros,
-            topic: '/cmd_vel'
-        });
-    }
-
-    // Add event listener for slider moves
-    robotSpeedRange = document.getElementById("robot-speed");
-    robotSpeedRange.oninput = function () {
-        teleop.scale = robotSpeedRange.value / 100
-    }
+    var body = document.getElementsByTagName('body')[0];
+    body.addEventListener('keydown', function(e) {
+        switch(e.keyCode) {
+            case 65: //left
+                twist.angular.z = speedAngular;
+                break;
+            case 68: //right
+                twist.angular.z = -speedAngular;
+                break;
+            case 87: ///up
+                twist.linear.x = speedLinear;
+                break;
+            case 83: //down
+                twist.linear.x = -speedLinear;
+        }
+    });
+    body.addEventListener('keyup', function(e) {
+        switch(e.keyCode) {
+            case 65: //left
+            case 68: //right
+                twist.angular.z = 0;
+                break;
+            case 87: ///up
+            case 83: //down
+                twist.linear.x = 0;
+        }
+    });
 }
 
 function createJoystick() {
-    // Check if joystick was aready created
+
     if (manager == null) {
         joystickContainer = document.getElementById('joystick');
-        // joystck configuration, if you want to adjust joystick, refer to:
-        // https://yoannmoinet.github.io/nipplejs/
-        var options = {
+
+        manager = nipplejs.create({
             zone: joystickContainer,
             position: { left: 50 + '%', top: 105 + 'px' },
             mode: 'static',
             size: 200,
             color: '#0066ff',
             restJoystick: true
-        };
-        manager = nipplejs.create(options);
-        // event listener for joystick move
-        manager.on('move', function (evt, nipple) {
-            // nipplejs returns direction is screen coordiantes
-            // we need to rotate it, that dragging towards screen top will move robot forward
-            var direction = nipple.angle.degree - 90;
-            if (direction > 180) {
-                direction = -(450 - nipple.angle.degree);
-            }
-            // convert angles to radians and scale linear and angular speed
-            // adjust if youwant robot to drvie faster or slower
-            var lin = Math.cos(direction / 57.29) * nipple.distance * 0.005;
-            var ang = Math.sin(direction / 57.29) * nipple.distance * 0.05;
-            // nipplejs is triggering events when joystic moves each pixel
-            // we need delay between consecutive messege publications to 
-            // prevent system from being flooded by messages
-            // events triggered earlier than 50ms after last publication will be dropped 
-            if (publishImmidiately) {
-                publishImmidiately = false;
-                moveAction(lin, ang);
-                setTimeout(function () {
-                    publishImmidiately = true;
-                }, 50);
-            }
         });
-        // event litener for joystick release, always send stop message
+
+        manager.on('move', function (evt, nipple) {
+
+            var lin = Math.sin(nipple.angle.radian) * nipple.distance * 0.01;
+            var ang = -Math.cos(nipple.angle.radian) * nipple.distance * 0.01;
+
+            twist.linear.x = lin * speedLinear;
+            twist.angular.z = ang * speedAngular;
+        });
+
         manager.on('end', function () {
-            moveAction(0, 0);
+            twist.linear.x = 0
+            twist.angular.z = 0
         });
     }
 }
 
+function initSliders() {
+    $('#lin-slider').slider({
+        tooltip: 'show',
+        min: 0,
+        max: 0.4,
+        step: 0.01,
+        value: 0.2
+    });
+    $('#lin-slider').on("slide", function(slideEvt) {
+        speedLinear = slideEvt.value;
+    });
+    speedLinear = 0.2
+
+    $('#ang-slider').slider({
+        tooltip: 'show',
+        min: 0,
+        max: 6.0,
+        step: 0.1,
+        value: 3.0
+    });
+    $('#ang-slider').on("slide", function(slideEvt) {
+        speedAngular = slideEvt.value;
+    });
+    speedAngular = 3.0
+}
+
+
+function publishTwist() {
+    //console.log("twist " + twist.linear.x + " " + twist.angular.z)
+    cmdVelPub.publish(twist)
+}
+
 window.onload = function () {
-    // determine robot address automatically
-    // robot_IP = location.hostname;
-    // set robot address statically
-    robot_IP = "10.0.0.1";
+    var robot_hostname = window.location.hostname;
 
     // // Init handle for rosbridge_websocket
     ros = new ROSLIB.Ros({
-        url: "ws://" + robot_IP + ":9090"
+        url: "ws://" + robot_hostname + ":9090"
     });
 
+    initSliders();
     initVelocityPublisher();
-    createJoystick();
     initTeleopKeyboard();
-    // get handle for video placeholder
-    // video = document.getElementById('video');
-    // // Populate video source 
-    // video.src = "http://" + robot_IP + ":8080/stream?topic=/camera/rgb/image_raw&type=mjpeg&quality=80";
-    // video.onload = function () {
-    //     // joystick and keyboard controls will be available only when video is correctly loaded
-    //     createJoystick();
-    //     initTeleopKeyboard();
-    // };
+    createJoystick();
+
+    setInterval(() => publishTwist(), 50);
+
 }
